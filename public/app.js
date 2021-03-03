37d50e8b82756267ceda9e3f8226b40681ac0241
EsraaAbuHana <esraa.abuhana@gmail.com>
2021-03-03 15:27:56 +0200

    done

---
+++ public/app.js
@@ -0,0 +1,10 @@
+'use strict';
+$('#updateForm').hide();
+$('#updateBtn').on('click',function() {$('#updateForm').show();
+$('#updateBtn').hide();   
+})
+
+
+// $('#hide').on('click',function() {$('#updateForm').hide();
+// $('#updateBtn').show();   
+// })
